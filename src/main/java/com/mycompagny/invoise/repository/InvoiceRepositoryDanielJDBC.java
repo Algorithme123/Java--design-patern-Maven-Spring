1e0e8ab5f961f7d3ae9ca701b424a0cf5a34aeb2
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2022-11-23 00:08:18 +0000

    Client Daniel BDD = JDBC

---
+++ src/main/java/com/mycompagny/invoise/repository/InvoiceRepositoryDanielJDBC.java
@@ -0,0 +1,19 @@
+package com.mycompagny.invoise.repository;
+
+import com.mycompagny.invoise.entity.Invoice;
+
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvoiceRepositoryDanielJDBC {
+
+    //Methode pour ajouter une facture
+    public  void create(Invoice invoice){
+       /* PreparedStatement preparedStatement = connexion.prepareStatement("INSERT INTO FACTURE (NUMBER, CUSTOMERNAME) VALUES (?,?')");
+
+        preparedStatement.executeUpdate();
+        */
+
+    }
+}
--- src/main/java/com/mycompagny/invoise/service/InvoiceServiceDaniel.java
+++ src/main/java/com/mycompagny/invoise/service/InvoiceServiceDaniel.java
@@ -2,17 +2,18 @@ package com.mycompagny.invoise.service;
 
 import com.mycompagny.invoise.entity.Invoice;
 import com.mycompagny.invoise.repository.InvoiceRepository;
+import com.mycompagny.invoise.repository.InvoiceRepositoryDanielJDBC;
 
 public class InvoiceServiceDaniel {
 
     private static long  lastNumber =112;
 
-    private InvoiceRepository invoiceRepository = new InvoiceRepository();
+    private InvoiceRepositoryDanielJDBC invoiceRepositoryDanielJDBC = new InvoiceRepositoryDanielJDBC();
 
 
 //    Methode pour creer une facture
     public void createInvoice(Invoice invoice){
         invoice.setNumero(String.valueOf("DNL_"+ (++lastNumber)));
-        invoiceRepository.create(invoice);
+        invoiceRepositoryDanielJDBC.create(invoice);
     }
 }
