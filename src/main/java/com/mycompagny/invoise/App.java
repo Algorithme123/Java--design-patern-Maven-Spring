783f2908460e14028f2fdce5bffe4597be0c7dcc
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2022-11-24 03:56:12 +0000

    principe de clean code , Test avec le nom des package et classe ...

--- src/main/java/com/mycompagny/invoise/App.java
+++ src/main/java/com/mycompagny/invoise/App.java
@@ -24,50 +24,34 @@ public class App
 
 
         Scanner sc = new Scanner(System.in) ;
-        System.out.println("Quel est le type de controller (keyboard , web, douchette)?");
-        String controllerType = sc.nextLine();
+        System.out.println("Quel est la classe de controller ?");
+        String controllerClass = sc.nextLine();
 
-        System.out.println("Quel est le type de service  (number , prefix)?");
-        String serviceType = sc.nextLine();
+        System.out.println("Quel est la classe du Service ?");
+        String serviceClass = sc.nextLine();
 
-        System.out.println("Quel est le type de repository (memory ,  database )?");
-        String repositoryType = sc.nextLine();
+        System.out.println("Quel est la classe repository ?");
+        String repositoryClass = sc.nextLine();
 
         InvoiceControllerInterface invoiceControllerInterface = null;
 
-        switch (controllerType){
 
-            case "keyboard":
-                invoiceControllerInterface = new InvoiceControllerKeyboard();
-                break;
-            case "web":
-                invoiceControllerInterface= new InvoiceControllerWeb();
-                break;
-                case "douchette":
-                    invoiceControllerInterface = new InvoiceControllerDouchette();
-         }
 
         InvoiceServiceInterface invoiceServiceInterface = null;
 
-        switch (serviceType){
-            case "number":
-                invoiceServiceInterface = new InvoiceServiceNumber();
-                break;
-            case "prefix":
-                invoiceServiceInterface = new InvoiceServicePrefix();
-                break;
-        }
+
 
         InvoiceRepositoryInterface invoiceRepositoryInterface = null;
 
-        switch (repositoryType){
-            case "memory":
-                invoiceRepositoryInterface = new InvoiceRepositoryMemory();
-                break;
-                case "database":
-                    invoiceRepositoryInterface = new InvoiceRepositoryDataBase();
-                    break;
-        }
+try {
+    invoiceControllerInterface = (InvoiceControllerInterface) Class.forName(controllerClass).getDeclaredConstructor().newInstance();
+    invoiceServiceInterface = (InvoiceServiceInterface) Class.forName(serviceClass).getDeclaredConstructor().newInstance();
+    invoiceRepositoryInterface = (InvoiceRepositoryInterface) Class.forName(repositoryClass).getDeclaredConstructor().newInstance();
+
+}
+catch (Exception e){
+    e.printStackTrace();
+}
         invoiceControllerInterface.setInvoiceServiceInterface(invoiceServiceInterface);
         invoiceServiceInterface.setInvoiceRepositoryInterface(invoiceRepositoryInterface);
         invoiceControllerInterface.createInvoice();
